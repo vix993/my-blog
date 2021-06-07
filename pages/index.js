c3748262c1a222856394349c8de228757ab13a1f
Victor <victor.as.nascimento@gmail.com>
2021-06-07 17:52:22 -0300

    erger

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,21 @@ export default function Home() {
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="" />
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-S0W1S87WNK"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+    
+              gtag('config', 'G-S0W1S87WNK');
+            `,
+          }}
+        />
+        <script>
+          
+        </script>
       </Head>
       <Carousel />
     </div>

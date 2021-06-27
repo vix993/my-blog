c7166974672d5271e74dd811bf8a6e26c4e07d8f
Victor <victor.as.nascimento@gmail.com>
2021-06-27 12:45:40 -0300

    adding filtering and project clicking

--- components/SiteView.jsx
+++ components/SiteView.jsx
@@ -4,21 +4,21 @@ import Icon from "@material-ui/core/Icon";
 
 export const SiteView = ({ currentSite }) => {
   return (
-    <main className="w-3/12 h-80 mx-auto rounded-2xl mt-4 transition hover:opacity-80 hover:brightness-70 duration-150">
+    <a className="w-3/12 h-80 mx-auto rounded-2xl mt-4 cursor-pointer transition hover:opacity-80 hover:brightness-70 duration-150 hover:text-gray-700" href={currentSite.url}>
       <div className="w-full h-full rounded-2xl">
         {currentSite.img && <img className="rounded-t-lg" src={currentSite.img} alt={currentSite.name} />}
 
         {/* <iframe src={currentSite.url} frameBorder="0" width="100%" height="auto"></iframe> */}
-        <footer className="bg-yellow-100 rounded-b-lg flex gap-2 p-2">
-          <a className="" href={currentSite.url} target="_blank">
-            <Icon className="hover:bg-yellow-500 rounded-lg"  component={OpenInNewIcon}/>
+        <footer className="bg-yellow-100 rounded-b-lg flex gap-2 p-2 justify-center items-center">
+          <a className="hover:text-yellow-500 flex items-center justify-center" href={currentSite.url} target="_blank">
+            <Icon component={OpenInNewIcon}/>
           </a>
-          <a href={currentSite.github} target="_blank">
-            <Icon className="hover:bg-yellow-500 rounded-lg"  component={GitHubIcon}/>
+          <a className="hover:text-yellow-500 flex items-center justify-center" href={currentSite.github} target="_blank">
+            <Icon component={GitHubIcon}/>
           </a>
           <span className="text-sm">{currentSite.description}</span>
         </footer>
       </div>
-    </main>
+    </a>
   );
 };

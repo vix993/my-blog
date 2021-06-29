9b203f21ed55bc78327bc0f5bfc4311c629e12b7
Victor <victor.as.nascimento@gmail.com>
2021-06-28 23:13:19 -0300

    new stuff

--- pages/_document.js
+++ pages/_document.js
@@ -15,7 +15,10 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@400;500;700&display=swap"
             rel="stylesheet"
           ></link>
-          <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/icon?family=Material+Icons"
+          />
 
           <script
             async

e421597a24bb93461416a9d3036178dac0ab34e6
Victor <victor.as.nascimento@gmail.com>
2021-06-29 00:22:41 -0300

    removing default js

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,5 @@
+import { useEffect } from 'react';
+
 import { Header } from "../components/Header";
 
 import { SkillsContextProvider } from "../contexts/SkillsContext";
@@ -5,6 +7,13 @@ import { SkillsContextProvider } from "../contexts/SkillsContext";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
   return (
     <SkillsContextProvider>
       <main className="w-screen h-screen">

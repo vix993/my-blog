595ef9ef886ba00f422a0abad72c644032499929
Victor <victor.as.nascimento@gmail.com>
2021-07-02 10:51:51 -0300

    upgrading to next 11

--- pages/_document.js
+++ pages/_document.js
@@ -9,16 +9,16 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+          {/* <link rel="preconnect" href="https://fonts.googleapis.com"></link>
           <link
             rel="preconnect"
             href="https://fonts.gstatic.com"
             crossOrigin
-          ></link>
+          ></link> */}
           <link
             href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@400;500;700&display=swap"
             rel="stylesheet"
-          ></link>
+          />
           <link
             rel="stylesheet"
             href="https://fonts.googleapis.com/icon?family=Material+Icons"

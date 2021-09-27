96aa72c67eb925977be793614cc591b996e9b14f
Victor Nascimento <52217652+vix993@users.noreply.github.com>
2021-09-27 15:46:37 -0300

    Update Intro.jsx

--- components/Intro.jsx
+++ components/Intro.jsx
@@ -4,7 +4,7 @@ export const Intro = ({}) => {
   return (
     <main>
       <div className="flex flex-col justify-center items-center gap-12 mt-16 rounded-2xl">
-        <h1>Software Developer</h1>
+        <h1>Junior Blockchain Developer</h1>
         <span className="text-2xl">
           Hi. I'm Victor, I enjoy building useful things.
         </span>

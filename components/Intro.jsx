6cd1eed37ee33f32a20093df1556c1a075da0904
Victor Nascimento <52217652+vix993@users.noreply.github.com>
2022-05-19 16:39:57 -0300

    Update Intro.jsx

--- components/Intro.jsx
+++ components/Intro.jsx
@@ -5,7 +5,7 @@ export const Intro = ({ }) => {
     <main>
       <div className="flex flex-col justify-center items-center gap-10 mt-16 rounded-2xl">
         <h1>Software Engineer</h1>
-        <span className="text-2xl flex items-center flex-col gap-4">
+        <span className="text-2xl flex items-center flex-col gap-4 text-center">
           Hi. I'm Victor, I enjoy building{" "}
           <Link href="/portfolio">
             <a className="rounded-md p-2 border border-yellow-500 bg-yellow-50 hover:text-gray-800 hover:bg-yellow-500">useful things</a>
